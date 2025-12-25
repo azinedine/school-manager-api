76fd0272c04003e57742a9a7201f48726cc9b6a3
azineddine <amarichezineddine@gmail.com>
2025-12-25 12:51:24 +0100

    Add migration for institutions table, defining schema with fields for name, code, type, wilaya_code, and municipality_id.

---
+++ database/migrations/2025_12_25_113207_create_institutions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('institutions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('code')->unique()->nullable(); // National ID of the school
+            $table->string('type')->default('school'); // primary, middle, high
+            $table->string('wilaya_code');
+            $table->string('municipality_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('institutions');
+    }
+};
