46da45beb45fe34a538443c9200a43ae20e75420
azineddine <amarichezineddine@gmail.com>
2025-12-25 12:51:01 +0100

    Add InstitutionResource class to transform institution data into JSON format for API responses.

---
+++ app/Http/Resources/InstitutionResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class InstitutionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'code' => $this->code,
+            'type' => $this->type,
+            'wilaya_code' => $this->wilaya_code,
+            'municipality_id' => $this->municipality_id,
+        ];
+    }
+}
