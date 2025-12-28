2a6417e19688be2a977e91c5abe0bdfd91aaae10
azineddine <amarichezineddine@gmail.com>
2025-12-28 10:10:31 +0100

    feat: Add last_login_at field to User model for tracking user login times

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,6 +53,7 @@ class User extends Authenticatable
         'weekly_teaching_load',
 
         'status',
+        'last_login_at',
     ];
 
     /**
@@ -74,6 +75,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
+            'last_login_at' => 'datetime',
             'password' => 'hashed',
             'subjects' => 'array',
             'levels' => 'array',
