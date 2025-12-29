13072ffccf47265854eda61394884b8a0c8890f4
azineddine <amarichezineddine@gmail.com>
2025-12-29 20:33:50 +0100

    refactor: update authorization check in StoreLessonPreparationRequest to use isTeacher method

--- app/Http/Requests/StoreLessonPreparationRequest.php
+++ app/Http/Requests/StoreLessonPreparationRequest.php
@@ -11,7 +11,7 @@ class StoreLessonPreparationRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return auth()->check() && auth()->user()->hasRole('teacher');
+        return auth()->check() && auth()->user()->isTeacher();
     }
 
     /**
