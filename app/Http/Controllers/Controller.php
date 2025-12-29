b23c8cfadb71f948e57c4f534ad7f8ec827a8c3b
azineddine <amarichezineddine@gmail.com>
2025-12-29 17:16:51 +0100

    feat: Add `AuthorizesRequests` trait to base controller to enable authorization capabilities.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+/**
+ * Base Controller
+ * 
+ * Provides authorization capabilities to all controllers.
+ * Uses Laravel's AuthorizesRequests trait for policy-based authorization.
+ */
 abstract class Controller
 {
-    //
+    use AuthorizesRequests;
 }
