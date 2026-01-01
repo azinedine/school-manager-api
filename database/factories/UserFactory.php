da496ec0f99077ccde7238cc40966adce7787644
azineddine <amarichezineddine@gmail.com>
2026-01-01 19:14:33 +0100

    feat: Implement dedicated seeders for admin, teacher, and institutions, streamline password assignment, and remove a migration for pedagogical fields.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,7 +30,7 @@ class UserFactory extends Factory
             'name' => $faker->name(),
             'email' => $faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= 'password',
             'remember_token' => Str::random(10),
         ];
     }
