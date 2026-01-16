eb19b7e48d33fe464918175c3696b3edae7f8437
azineddine <amarichezineddine@gmail.com>
2026-01-16 21:10:48 +0100

    style: remove unused import in TeachingMethodSeeder for improved readability

--- database/seeders/TeachingMethodSeeder.php
+++ database/seeders/TeachingMethodSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\TeachingMethod;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class TeachingMethodSeeder extends Seeder
