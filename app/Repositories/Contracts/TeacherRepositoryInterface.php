98c2b5b14f0ed0ac9422dbe39b49475eae7fcccb
azineddine <amarichezineddine@gmail.com>
2026-01-16 21:06:23 +0100

    style: clean up whitespace in TeacherRepositoryInterface.php for improved readability

--- app/Repositories/Contracts/TeacherRepositoryInterface.php
+++ app/Repositories/Contracts/TeacherRepositoryInterface.php
@@ -7,7 +7,7 @@ interface TeacherRepositoryInterface
     /**
      * Get all teachers with pagination.
      *
-     * @param int $perPage
+     * @param  int  $perPage
      * @return \Illuminate\Pagination\LengthAwarePaginator
      */
     public function getAllTeachers($perPage = 20);
